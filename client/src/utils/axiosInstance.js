8a3e5423ea679597aab8301846189c93522fc7c4
Roniwilliams <ronideni1220@gmail.com>
2025-11-03 12:49:26 +0530

    added access api

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://code-doc-ai-backend-server.vercel.app'
+const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://code-doc-ai-backend-server.vercel.app/api'
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
